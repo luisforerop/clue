ffd1d69c6d4aa0b619691976cca6aef5a43a8709
Luis Felipe Forero Penaloza <luis.forero@xpertgroup.co>
2023-12-06 19:08:20 -0500

    fix: :adhesive_bandage: remove unused imports

--- src/screens/AddWords/index.tsx
+++ src/screens/AddWords/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const AddWords = () => {
   return <div>AddWords</div>
 }
--- src/screens/CompleteStepByStep/index.tsx
+++ src/screens/CompleteStepByStep/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const CompleteStepByStep = () => {
   return <div>CompleteStepByStep</div>
 }
